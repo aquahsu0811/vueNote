acc2c0db8b2f98e3381ea117c8de10e8584499d1
aquahsu0811 <bellems811@gmail.com>
2022-12-03 12:39:08 +0800

    update

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <base-card>
+    <template #header>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+    </template>
+    <template v-slot:default>
+      <p>{{ infoText }}</p>
+    </template>
+  </base-card>
+</template>
+
+<script>
+import BaseCard from "./BaseCard.vue";
+
+export default {
+  components: {
+    "base-card": BaseCard,
+  },
+  props: ["fullName", "infoText", "role"],
+};
+</script>
