4e17b5213794b0b0cd4adc80086769c0bbb50ffd
aquahsu0811 <bellems811@gmail.com>
2022-12-04 23:50:42 +0800

    update

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,84 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
+      </div>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        NOT Found...
+      </p> 
+      <ul v-else-if="!isLoading && results && results.length > 0">
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.error = null;
+      this.isLoading = true;
+      console.log('test');
+      fetch('https://vue-http-faaf1-default-rtdb.firebaseio.com/aurveys.json')
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log('error:',error);
+          this.isLoading = false;
+          this.error = 'Failed to fetch data - please try again later.';
+        });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
